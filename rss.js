8e0fdaf6cdee73ea1344594b2247125882eb0813
temp478 <185asile@gmail.com>
2025-04-22 22:18:57 +0200

    Create rss.js

---
+++ rss.js
@@ -0,0 +1,45 @@
+<!-- start sw-rss-feed code --> 
+<script type="text/javascript"> 
+<!-- 
+rssfeed_url = new Array(); 
+rssfeed_url[0]="https://fetchrss.com/rss/6807d4849cf6660e4f076f436807d45a1a9625888e076514.rss";  
+rssfeed_frame_width="230"; 
+rssfeed_frame_height="260"; 
+rssfeed_scroll="on"; 
+rssfeed_scroll_step="10"; 
+rssfeed_scroll_bar="off"; 
+rssfeed_target="_blank"; 
+rssfeed_font_size="11"; 
+rssfeed_font_face="Helvetica"; 
+rssfeed_border="off"; 
+rssfeed_css_url=""; 
+rssfeed_title="on"; 
+rssfeed_title_name="🌱NEWS🌱"; 
+rssfeed_title_bgcolor="#fff"; 
+rssfeed_title_color="#000"; 
+rssfeed_title_bgimage=""; 
+rssfeed_footer="off"; 
+rssfeed_footer_name=""; 
+rssfeed_footer_bgcolor="#fff"; 
+rssfeed_footer_color="#333"; 
+rssfeed_footer_bgimage=""; 
+rssfeed_item_title_length="200"; 
+rssfeed_item_title_color="#000"; 
+rssfeed_item_bgcolor="#fff"; 
+rssfeed_item_bgimage=""; 
+rssfeed_item_border_bottom="on"; 
+rssfeed_item_source_icon="off"; 
+rssfeed_item_date="off"; 
+rssfeed_item_description="off"; 
+rssfeed_item_description_length="120"; 
+rssfeed_item_description_color="#666"; 
+rssfeed_item_description_link_color="#333"; 
+rssfeed_item_description_tag="off"; 
+rssfeed_no_items="0"; 
+rssfeed_cache = "d5d2eb9a031ece16cd39ba6b0f01ac69"; 
+//--> 
+</script> 
+<script type="text/javascript" src="//feed.surfing-waves.com/js/rss-feed.js"></script> 
+<!-- The link below helps keep this service FREE, and helps other people find the SW widget. Please be cool and keep it! Thanks. --> 
+<div style="color:#ccc;font-size:10px; text-align:right; width:230px;">powered by <a href="https://surfing-waves.com" rel="noopener" target="_blank" style="color:#ccc;">Surfing Waves</a></div> 
+<!-- end sw-rss-feed code -->
